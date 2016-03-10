7e3cf356c9548032a13e586c16495d14898e481c
greatwillow <greg_denys@yahoo.com>
2016-03-10 14:33:21 -0500

    -A

---
+++ src/main.js
@@ -0,0 +1,44 @@
+var React = require('react-native');
+var {
+    View,
+    Text,
+    StyleSheet, 
+    Navigator
+} = React;
+
+var Signin = require('./components/authentication/signin');
+var Signup = require('./components/authentication/signup');
+
+var ROUTES = {
+  signin: Signin,
+  signup: Signup
+};
+
+
+module.exports = React.createClass({
+    
+    renderScene: function(route, navigator) {
+        var Component = ROUTES[route.name]; 
+        return <Component route={route} navigator={navigator} />;
+    },    
+    render: function() {
+        return (
+                <Navigator 
+                    style={styles.container} 
+                    initialRoute = {{name: 'signin'}}
+                    renderScene = {this.renderScene}
+                    configureScene = {() => {return Navigator.SceneConfigs.FloatFromRight; }}
+                />
+
+           
+            
+                
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
